3441868ca4db3f1381dc87780e0045a5da87beba
Matias Brula <mbrula@itba.edu.ar>
2020-06-16 15:41:40 -0300

    add command args for ipv4 port and address

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@
 #include "args.h"
 
 /** TODO: SACAR CUANDO CORRIJAMOS lo de char * a  */
-#define IPV4_ADDRESS    INADDR_ANY
+#define IPV4_ADDRESS     "0.0.0.0" //INADDR_ANY
 #define IPV6_ADDRESS    "::"
 
 #define USERS_FILENAME  "users.txt"
@@ -30,7 +30,7 @@
 
 enum socket_errors { socket_no_error, error_socket_create, error_socket_bind, error_socket_listen, error_invalid_address};
 
-static unsigned create_socket_ipv4(uint32_t address, unsigned port, int * server_fd);
+static unsigned create_socket_ipv4(const char *  address, unsigned port, int * server_fd);
 static unsigned create_socket_ipv6(const char * address, unsigned port, int * server_fd);
 static const char * socket_error_description(enum socket_errors error);
 static const char * file_error_description(enum file_errors error);
@@ -63,13 +63,12 @@ main(const int argc, const char **argv) {
 
     int server_ipv4, server_ipv6;
     
-    /** TODO: Reemplazar la de abajo con esta cuando arreglemos eso  */
-    // enum socket_errors error_ipv4 = create_socket_ipv4(args.socks_addr, args.socks_port, &server_ipv4);
-    enum socket_errors error_ipv4 = create_socket_ipv4(IPV4_ADDRESS, args.socks_port, &server_ipv4);
+    enum socket_errors error_ipv4 = create_socket_ipv4(args.socks_addr, args.socks_port, &server_ipv4);
     if (error_ipv4 != socket_no_error) {
         err_msg = socket_error_description(error_ipv4);
         goto finally;
     }
+    // TODO check ipv6 address handling by args
     enum socket_errors error_ipv6 = create_socket_ipv6(IPV6_ADDRESS, args.socks_port, &server_ipv6);
     if (error_ipv6 != socket_no_error) {
         err_msg = socket_error_description(error_ipv6);
@@ -174,12 +173,14 @@ finally:
 }
 
 static unsigned 
-create_socket_ipv4(uint32_t address, unsigned port, int * server_fd) {
+create_socket_ipv4(const char * address, unsigned port, int * server_fd) {
     struct sockaddr_in addr;
     memset(&addr, 0, sizeof(addr));
     addr.sin_family      = AF_INET;
-    addr.sin_addr.s_addr = htonl(address);
     addr.sin_port        = htons(port);
+    if (inet_pton(AF_INET, address, &addr.sin_addr) == 0) {
+        return error_invalid_address;
+    }
 
     *server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (*server_fd < 0) {
