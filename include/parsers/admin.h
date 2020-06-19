d25020b714661ebda7c1bc756329ff407a6f82ab
Matias Brula <mbrula@itba.edu.ar>
2020-06-19 16:31:25 -0300

    delete reply protocol comments

--- include/parsers/admin.h
+++ include/parsers/admin.h
@@ -23,21 +23,6 @@
 			~ CLIENT	X'00'
 			~ ADMIN		X'01'
 
-    Reply
-        +-----+--------+
-        | CMD | STATUS |
-        +-----+--------+
-        |  1  |   1    |
-        +-----+--------+
-
-	Where: 
-		- CMD		X'01'
-		- STATUS	-> answer status
-			~ SUCCESS			X'00'
-			~ INV UTYPE			X'02'
-			~ GEN SERVER FAIL	X'FF'
-
-
  * ******************** Delete User ********************
     +-----+------+----------+
     | CMD | ULEN |   UNAME  |
@@ -50,20 +35,6 @@
 		- ULEN		-> length of the UNAME field
 		- UNAME 	-> username to remove
 
-    Reply:
-    +-----+--------+
-    | CMD | STATUS |
-    +-----+--------+
-    |  1  |   1    |
-    +-----+--------+
-
-	Where: 
-		- CMD		X'02'
-		- STATUS	-> answer status
-			~ SUCCESS			X'00'
-			~ GEN SERVER FAIL	X'FF'
-
-
 * ******************** List Usernames ********************
     +-----+
     | CMD |
@@ -73,28 +44,6 @@
 	
 	Where:
 		- CMD		X'03'
-
-    Reply:
-	+-----+--------+--------+------+----------+-------+
-	| CMD | STATUS | NUSERS | ULEN |   UNAME  | UTYPE |
-	+-----+--------+--------+------+----------+-------+
-	|  1  |   1    |   2    |  1   | 1 to 255 |   1   |
-	+-----+--------+--------+------+----------+-------+
-							[------------|------------]
-									NUSERS times
-
-	Where: 
-		- CMD		X'03'
-		- STATUS	-> answer status
-			~ SUCCESS			X'00'
-			~ GEN SERVER FAIL	X'FF'
-		- NUSERS	-> user quantity in "network octet order"
-		- ULEN		-> length of the UNAME field
-		- UNAME		-> username
-		- UTYPE 	-> user type
-			~ CLIENT	X'00'
-			~ ADMIN		X'01'
-
 
 * ******************** Get Metrics ********************
     +-----+--------+
@@ -109,27 +58,6 @@
 			~ HISTORIC CONNECTIONS		X'00'
 			~ CONCURRENT CONNECTIONS	X'01'
 			~ HISTORIC BYTES TRANSF		X'02'
-	
-    Reply:
-    +-----+--------+--------+------+----------+
-    | CMD | STATUS | METRIC | VLEN |   VALUE  |
-    +-----+--------+--------+------+----------+
-    |  1  |   1    |   1    |  1   | 0 to 255 |
-    +-----+--------+--------+------+----------+
-
-	Where: 
-		- CMD		X'04'
-		- STATUS	-> status answer
-			~ SUCCESS			X'00'
-			~ INV METRIC		X'03'
-			~ GEN SERVER FAIL	X'FF'
-		- METRIC 	-> requested metric
-			~ HISTORIC CONNECTIONS		X'00'
-			~ CONCURRENT CONNECTIONS	X'01'
-			~ HISTORIC BYTES TRANSF		X'02'
-		- VLEN 		-> length of the VALUE filed
-		- VALUE 	-> value of the requested metric
-
 
 * ******************** Get Configurations ********************
     +-----+--------+
@@ -146,28 +74,6 @@
 			~ BUFFER WRITE		X'02'
 			~ SELECT TIMEOUT	X'03'
 	
-    Reply:
-    +-----+--------+--------+------+----------+
-    | CMD | STATUS | CONFIG | VLEN |   VALUE  |
-    +-----+--------+--------+------+----------+
-    |  1  |   1    |   1    |  1   | 0 to 255 |
-    +-----+--------+--------+------+----------+
-
-	Where: 
-		- CMD		X'05'
-		- STATUS	-> answer status
-        	~ SUCCESS			X'00'
-			~ INV CONFIG		X'04'
-			~ GEN SERVER FAIL	X'FF'
-		- CONFIG 	-> requested config
-			~ BUFFER SIZE BOTH  X'00'		
-			~ BUFFER READ		X'01'
-			~ BUFFER WRITE		X'02'
-			~ SELECT TIMEOUT	X'03'
-		- VLEN 		-> length of the VALUE filed
-		- VALUE 	-> value of the requested config
-
-
  * ******************** Change Configurations ********************  
     +-----+--------+------+----------+
     | CMD | CONFIG | VLEN |   VALUE  |
@@ -185,38 +91,6 @@
 		- VLEN		-> length of the VALUE filed
 		- VALUE 	-> value of the requested config
 
-    Reply (for valid configuration):
-    +-----+--------+--------+
-    | CMD | STATUS | CONFIG |
-    +-----+--------+--------+
-    |  1  |   1    |   1    |
-    +-----+--------+--------+
-
-	Where: 
-		- CMD		X'06'
-		- STATUS	-> answer status
-			~ SUCCESS			X'00'
-			~ INV CONFIG		X'04'
-			~ INV VALUE 		X'05'
-			~ GEN SERVER FAIL	X'FF'
-		- CONFIG 	-> requested config
-			~ BUFFER SIZE BOTH  X'00'		
-			~ BUFFER READ		X'01'
-			~ BUFFER WRITE		X'02'
-			~ SELECT TIMEOUT	X'03'
-
-    Reply(for invalid command)
-    +-----+--------+
-    | CMD | STATUS |
-    +-----+--------+
-    |  1  |   1    |
-    +-----+--------+
-
-	Where: 
-		- CMD		-> requested command
-        - STATUS	-> answer status
-			~ INV CMD			X'01'
-			~ GEN SERVER FAIL	X'FF'
 */
 
 
