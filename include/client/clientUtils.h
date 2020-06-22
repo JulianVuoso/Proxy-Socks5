2e8032152a076e647f181e12b22861cefd16b816
gbarbieri <gbarbieri@itba.edu.ar>
2020-06-21 23:35:37 -0300

    fixed missing declaration in header file

--- include/client/clientUtils.h
+++ include/client/clientUtils.h
@@ -30,5 +30,6 @@
 void validateArgv(int argc,char *const*argv);
 int getNextCommand(int argc,char * const*argv,int *cmdStartIndex,uint8_t *data,int *datalen);
 int handleResponse(int sockfd,int cmd, uint8_t *readBuffer);
+void recvWrapper(int sockfd,void *buffer, size_t len, int flags);
 
 #endif
