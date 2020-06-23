2e96ea8233dd11b7f86748ccbcf7d56c52f692be
JulianVuoso <juvuoso@itba.edu.ar>
2020-06-23 13:56:28 -0300

    Supressed warning

--- src/utils/args.c
+++ src/utils/args.c
@@ -51,18 +51,7 @@ add_user_client(char *s) {
         exit(1);
     }
     strcpy((char *)pass, (char *)token); 
-    /***/
-    
-    /* if (token == NULL) {
-        pass = malloc(2);
-        if(pass == NULL) exit(1);
-        strcpy((char *)pass, "");
-    } else { 
-        pass = malloc(strlen((const char *) token) + 1);
-        if(pass == NULL) exit(1);
-        strcpy((char *)pass, (char *)token);
-    } */
-    /***/
+
     add_user_to_list(user, pass, user_client);
     free(user);
     free(pass);
@@ -73,51 +62,6 @@ static void
 add_user(char* s) {
     uint8_t * token = (uint8_t *)strtok(s, SEPARATOR), *user, *pass, state = read_user, level;
     
-    /* while(state != read_done){
-        switch (state){
-            case read_user: 
-                if(token == NULL){
-                    fprintf(stderr, "user not found");
-                    exit(1);
-                }
-                user = malloc(strlen((const char *) token) + 1);
-                if (user == NULL) exit(1);
-                strcpy((char *)user, (char *)token); 
-                state = read_pass; 
-                break;
-            case read_pass: 
-                if (token == NULL) {
-                    pass = malloc(2);
-                    if(pass == NULL) exit(1);
-                    strcpy((char *)pass, "");
-                } else { 
-                    pass = malloc(strlen((const char *) token) + 1);
-                    if(pass == NULL) exit(1);
-                    strcpy((char *)pass, (char *)token);
-                }
-                state = read_type; 
-                break;
-            case read_type:
-                if(token == NULL){
-                    fprintf(stderr, "user level not found\n");        
-                    exit(1);
-                }
-                level = atoi((char *) token); 
-                if((level != user_client && level != user_admin) || (!isdigit(*token))){
-                    fprintf(stderr, "invalid user level (0:client 1:admin) \n");        
-                    exit(1);
-                }
-                enum file_errors err = add_user_to_list(user, pass, level);
-                free(user);
-                free(pass);
-                if (err == memory_heap) exit(1); 
-                state = read_done; 
-                break;
-            default: break;
-        }
-        token = (uint8_t *)strtok(NULL, SEPARATOR);
-    }
- */
     while(token)
     {
         switch (state)
